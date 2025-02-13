96eae3853fdd86306e7b829b95863b58d400fd4a
Juliano Quites <juliano.quites@gmail.com>
2025-02-13 00:08:32 -0300

    update

--- API/e2e/mock.test.js
+++ API/e2e/mock.test.js
@@ -12,7 +12,7 @@ const BASE_URL = "https://jsonplaceholder.typicode.com";
 
 describe("Testes com jest.mock para HTTP status de 400", () => {
   it("Deve retornar status 400 ao tentar criar um post sem campos obrigatórios", async () => {
-    const invalidPost = { title: "Apenas título" }; // Falta body e userId
+    const invalidPost = { title: "Apenas título" };
     const response = await request(BASE_URL).post("/posts").send(invalidPost);
     expect(response.status).toBe(400);
     expect(response.body.error).toBe("Campos obrigatórios faltando");
